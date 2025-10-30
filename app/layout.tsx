d30391d4f97ae9719a2290355c5b53e1c754b54e
ShadowOfNaka <83060273+WebShardow@users.noreply.github.com>
2025-10-30 09:26:04 +0000

    ลบ // @ts-expect-error Missing type definitions for next/font/google

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-// @ts-expect-error Missing type definitions for next/font/google
 import "./globals.css";
 
 import { Header } from '@/components/Header';
